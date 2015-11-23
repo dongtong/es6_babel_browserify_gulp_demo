98ba8a9ddc81e2e6eb5a1c75acdf536e3a0835db
djh <jinhu.tung@gmail.com>
2015-11-23 19:46:14 +0800

    Add ajaxReq implement

--- src/javascripts/common/util.js
+++ src/javascripts/common/util.js
@@ -9,7 +9,64 @@ let Util = {
   * @example Util.ajaxReq(object)
   */
   ajaxReq: function(options){
+      var retryTimes = Const.RETRY_TIMES;
 
+      function req(){
+            $.ajax({
+                type: options.type || "GET",
+                url: options.url,
+                data: options.data || null,
+                cache: false,
+                timeout: Const.TIME_OUT,
+                crossDomain: true,
+                dataType: "jsonp",
+                jsonpCallback: options.jsonpCallback || null,
+                contentType: "application/json;utf-8", 
+                success: function(data, status, xhr){
+                    if(typeof options.successFn === 'function') {
+                        options.successFn(data);  
+                    }
+                },
+                error: function(xhr, status, errorThrown){
+                    //timeout异常交给complete处理
+                    if(status !== 'timeout' && !options.observeRes()) {
+                        if(typeof options.errorFn === 'function') {
+                            options.errorFn();  
+                        }else{
+                            Util.redirectToErr('0');
+                        }
+                    }
+                },
+                complete: function(xhr, status) {
+                    //status: success,notmodified,nocontent,error,timeout,abort,parsererror
+                    if(status === 'timeout' && !options.observeRes()) {
+                        //console.log('retry-',retryTimes)
+                        if(options.retry === undefined || options.retry) {
+                            if(retryTimes > 0) {
+                                retryTimes = retryTimes - 1;
+                                req();
+                            } else {
+                                if(options.timeoutFn){
+                                    options.timeoutFn();
+                                }else{
+                                    Util.redirectToErr('1');
+                                }
+                            }
+                        } else {
+                            if(options.timeoutFn){
+                                options.timeoutFn();
+                            }else{
+                                Util.redirectToErr('1');
+                            }
+                        }
+                        
+                    }
+
+                }
+            });
+      }
+
+      req();
   }
 }
 
